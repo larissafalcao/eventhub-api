765143df69a87132bc911031aca89d450754dfce
Larissa Falcão <larissafalcao@outlook.com>
2026-02-16 18:25:29 -0300

    feat: implements open api config

---
+++ src/main/java/com/larissafalcao/eventhub_api/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.larissafalcao.eventhub_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI eventHubOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Event Hub API")
+                        .description("API documentation for Event Hub")
+                        .version("v1"));
+    }
+}
